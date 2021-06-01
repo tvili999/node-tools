c8ffc793691877708cf9fe55c2c542944b973c47
Takacs Vili <tvili999@gmail.com>
2021-06-01 03:51:54 +0200

    stash implementation

---
+++ stash/index.js
@@ -0,0 +1,46 @@
+const arrayMap = require("./arrayMap");
+
+const createStash = () => {
+    const queryMakers = {};
+
+    return {
+        create: () => {
+            let values = {};
+
+            const stash = new Proxy({}, {
+                get: (queries, name) => {
+                    if(!queries[name]) {
+                        const queryMaker = queryMakers[name];
+                        if(!queryMaker)
+                            throw `Query ${name} does not exist`;
+
+                        queries[name] = async (...args) => {
+                            const key = [...args];
+                            if(!values[name])
+                                values[name] = arrayMap();
+                            
+                            const cachedValue = values[name].get(key);
+                            if(cachedValue)
+                                return cachedValue;
+                            
+                            const value = await Promise.resolve(queryMaker(stash, ...args));
+                            values[name].set(key, value);
+                            return value;
+                        };
+                    }
+                    return queries[name];
+                }
+            });
+
+            return stash;
+        },
+        addQuery: (name, queryMaker) => {
+            if(name in queryMakers)
+                throw `Query ${name} already exists in stash`;
+            
+            queryMakers[name] = queryMaker;
+        }
+    }
+};
+
+module.exports = createStash;
--- stash/stash.test.js
+++ stash/stash.test.js
@@ -1,3 +1,49 @@
+const createStash = require("./index");
+
 test('passes', () => {
     expect(true).toBe(true);
-})
+});
+
+test('query cannot be added twice', () => {
+    const stash = createStash();
+    stash.addQuery("query", () => {});
+
+    expect(() => {
+        stash.addQuery("query", () => {});
+    }).toThrow();
+});
+
+test('get a value', async () => {
+    const stash = createStash();
+
+    stash.addQuery('query', (stash, id) => id);
+
+    const requestStash = stash.create();
+    expect(await requestStash.query(15)).toBe(15);
+});
+
+test('call maker only once', async () => {
+    const stash = createStash();
+
+    const maker = jest.fn((stash, id) => id);
+    stash.addQuery('query', maker);
+
+    const requestStash = stash.create();
+    expect(await requestStash.query(15)).toBe(15);
+    expect(await requestStash.query(15)).toBe(15);
+    expect(maker.mock.calls.length).toBe(1);
+});
+
+test('call maker twice for two different values', async () => {
+    const stash = createStash();
+
+    const maker = jest.fn((stash, id) => id);
+    stash.addQuery('query', async (...args) => maker(...args));
+
+    const requestStash = stash.create();
+    expect(await requestStash.query(15)).toBe(15);
+    expect(await requestStash.query(15)).toBe(15);
+    expect(await requestStash.query(27)).toBe(27);
+    expect(await requestStash.query(27)).toBe(27);
+    expect(maker.mock.calls.length).toBe(2);
+});
