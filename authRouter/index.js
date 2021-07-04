ce55c74c2e51884572c4af23c00a47389027e0b3
Takacs Vili <tvili999@gmail.com>
2021-07-04 19:28:23 +0200

    added extra args functionality to authRouter

--- authRouter/index.js
+++ authRouter/index.js
@@ -1,13 +1,18 @@
 const createAuthRouter = (states) => {
     let _states = {};
     for(const stateName in states) {
-        _states[stateName] = handler => async (req, res, next, ...args) => {
-            const passes = await Promise.resolve(states[stateName](req));
-            if(passes) {
-                handler(req, res, next, ...args);
-            }
-            else {
-                next();
+        _states[stateName] = (...handlerArgs) => {
+            handlerArgs = [...handlerArgs];
+            const handler = handlerArgs.pop();
+
+            return async (req, res, next, ...args) => {
+                const passes = await Promise.resolve(states[stateName](req, ...handlerArgs));
+                if(passes) {
+                    handler(req, res, next, ...args);
+                }
+                else {
+                    next();
+                }
             }
         }
     }
