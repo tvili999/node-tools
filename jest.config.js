d48ca4a68cba62948f26de41ae56c7bd98263e1a
Vilmos Takacs <vilmos.takacs@aliasinnov.com>
2023-07-31 00:56:30 +0200

    Reverting to CommonJS

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
   moduleNameMapper: {
-    '(.+)\\.js': '$1'
+    "(.+)\\.js": "$1",
   },
 };
